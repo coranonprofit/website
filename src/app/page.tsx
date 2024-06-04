740f667838ef7eb2195051b2d13a0ccf8b52a546
michael-m-2983 <michaelmccright2983@gmail.com>
2024-06-04 11:57:55 -0500

    Add page number support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Canvas, useFrame, ThreeElements, Vector3, useLoader } from "@react-three/fiber";
 import React from "react"
 import * as THREE from 'three'
-import { Environment, OrbitControls, ScrollControls, useScroll } from "@react-three/drei";
+import { Environment, OrbitControls, ScrollControls, useScroll, Text } from "@react-three/drei";
 import "./page.css";
 
 function ExampleCube(props: ThreeElements['mesh']) {
@@ -11,6 +11,13 @@ function ExampleCube(props: ThreeElements['mesh']) {
 
   const [hovered, hover] = React.useState(false);
   const [clicked, click] = React.useState(false);
+
+  const [X, setX] = React.useState<number>(0);
+  const scroll = useScroll();
+
+  useFrame((state, delta) => {
+    setX(scroll.offset * scroll.pages);
+  });
 
   return (
     <mesh
@@ -21,7 +28,7 @@ function ExampleCube(props: ThreeElements['mesh']) {
       onPointerOver={() => hover(true)}
       onPointerOut={() => hover(false)}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'green' : 'red'} />
+      <meshStandardMaterial color={new THREE.Color(`rgb(${20 * Math.floor(X)}%, 50%, 50%)`)} />
     </mesh>
   )
 }
@@ -47,19 +54,22 @@ function ImagePane(props: ImagePaneProps) {
 function Scene() {
   const scroll = useScroll();
   const [x, setX] = React.useState<number>(0);
+  const [page, setPage] = React.useState<number>(0);
 
   useFrame((state, delta) => {
     setX(scroll.offset);
+    setPage(Math.max(Math.ceil(scroll.offset * scroll.pages), 1));
   });
 
   return <>
+    <Text position={[0, 0, 3]}>Page {page}</Text>
     <ImagePane src="/logo.png" position={[-3.6, x, -x]} />
-    <ExampleCube position={[-2.4, x, 2 * x]} />
-    <ExampleCube position={[-1.2, 0, x * x]} />
+    {page >= 1 && <ExampleCube position={[-2.4, x, 2 * x]} />}
+    {page >= 2 && <ExampleCube position={[-1.2, 0, x * x]} />}
     <ImagePane src="/photos/photo1.jpg" position={[0, 0, 0]} width={(1 + scroll.offset) * 1.54} height={(1 + scroll.offset) * 1} />
-    <ExampleCube position={[1.2, 0, -x]} />
-    <ExampleCube position={[2.4, x, 2 * x]} />
-    <ExampleCube position={[3.6, -x, 3 * x]} />
+    {page >= 3 && <ExampleCube position={[1.2, 0, -x]} />}
+    {page >= 4 && <ExampleCube position={[2.4, x, 2 * x]} />}
+    {page == 5 && <ExampleCube position={[3.6, -x, 3 * x]} />}
   </>;
 }
 
