f0a4864974b92c0bfd60fb5fec877c74ebfe1f5e
michael-m-2983 <michaelmccright2983@gmail.com>
2024-06-03 22:07:51 -0500

    Add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,7 @@
+"use client";
+import '@mantine/core/styles.css';
+import { AppShell, createTheme, Group, MantineProvider, Image, Text } from '@mantine/core';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +9,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MantineProvider>
+          <PageLayout>
+            {children}
+          </PageLayout>
+        </MantineProvider>
+      </body>
     </html>
   );
+}
+
+function PageLayout(props: {
+  children: any
+}) {
+  return <AppShell>
+    <AppShell.Header>
+      <Group px="md" justify='space-between' p="md">
+        <Group>
+          <Image src="/logo.png" w={"xl"} />
+          <Text>CORA</Text>
+        </Group>
+        <Group gap="xl">
+          {["Home", "About", "etc."].map(pageName => <Text key={pageName}>
+            {pageName}
+          </Text>)}
+        </Group>
+      </Group>
+    </AppShell.Header>
+
+    <AppShell.Main>{props.children}</AppShell.Main>
+  </AppShell>
 }
