cdb1e989d66225ff329687bdbe0abe52f85af833
michael-m-2983 <michaelmccright2983@gmail.com>
2024-07-05 17:44:14 -0500

    Add account page redirect

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -2,9 +2,15 @@
 
 import { Center, Container, Fieldset, Stack, Text, Title } from "@mantine/core";
 import { useSession } from "next-auth/react"
+import { redirect } from "next/navigation";
 
 export default function AccountPage() {
     const { data: session } = useSession();
+
+
+    if(!session) {
+        redirect("/api/auth/signin");
+    }
 
     return <Container>
         <Center>
