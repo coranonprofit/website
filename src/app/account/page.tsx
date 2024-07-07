2bf6e6aa6276015dae40cae5561e2fa2f9821cb2
michael-m-2983 <michaelmccright2983@gmail.com>
2024-07-07 16:40:33 -0500

    Add branch display to account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,14 +3,21 @@
 import { Center, Container, Fieldset, Stack, Text, Title } from "@mantine/core";
 import { useSession } from "next-auth/react"
 import { redirect } from "next/navigation";
+import { getBranchName } from "./account_info";
+import React from "react";
 
 export default function AccountPage() {
     const { data: session } = useSession();
 
+    const [branchName, setBranchName] = React.useState<string | undefined>(undefined);
 
     if(!session) {
         redirect("/api/auth/signin");
     }
+
+    React.useEffect(() => {
+        getBranchName(session?.user?.email!).then(setBranchName);
+    }, []);
 
     return <Container>
         <Center>
@@ -21,6 +28,7 @@ export default function AccountPage() {
             <Stack>
                 <Text><b>Name</b>: {session?.user?.name}</Text>
                 <Text><b>Email</b>: {session?.user?.email}</Text>
+                {branchName ? <Text><b>Branch</b>: {branchName}</Text> : <Text><b>Loading branch info...</b></Text>}
             </Stack>
         </Fieldset>
     </Container>
