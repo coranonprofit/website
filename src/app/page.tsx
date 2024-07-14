49c634ce3c9126ad7390fe123a9b3c9fdfdf43df
spoopr <spoopr@users.noreply.github.com>
2024-07-14 16:15:52 -0500

    added show/hide transition to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,103 +12,127 @@ import { BranchesMenuContext } from "@/lib/context";
 
 export default function MainPage() {
   return (
-    <>
-      <Hero />
+      <>
+          <Hero />
 
-      <NavBar />
+          <NavBar />
 
-      <main>
-        <Container>
-          <div className={styles.content}>
-            <Image src="/images/group1.jpg" alt="Group photo 1" fill /> 
-          </div>
+          <main>
+              <hr className={styles.spacer} />
+              <Container>
+                  <div className={styles.content}>
+                      <Image
+                          src="/images/group1.jpg"
+                          alt="Group photo 1"
+                          fill
+                      />
+                  </div>
 
-          <div className={styles.text}>
-            <h1>100%</h1>
-            <h2>Student Lead</h2>
-            <p>
-              100% of all events, projects, meetings, and ideas are initiated and
-              run by our amazing student leaders!
-            </p>
-          </div>
-        </Container>
+                  <div className={styles.text}>
+                      <h1>100%</h1>
+                      <h2>Student Lead</h2>
+                      <p>
+                          100% of all events, projects, meetings, and ideas are
+                          initiated and run by our amazing student leaders!
+                      </p>
+                  </div>
+              </Container>
 
-        <Container>
-          <div className={styles.text}>
-            <h1>250+ Students</h1>
-            <h2>One goal</h2>
-            <p>
-              All CORA students are dedicated to our mission, to improve our
-              communities and make an impact on the world.
-            </p>
-          </div>
-          
-          <div className={styles.content}>
-            <Image src="/images/photos/group2.jpg" alt="Group photo 2" fill />
-          </div>
-        </Container>
+              <Container>
+                  <div className={styles.text}>
+                      <h1>250+ Students</h1>
+                      <h2>One goal</h2>
+                      <p>
+                          All CORA students are dedicated to our mission, to
+                          improve our communities and make an impact on the
+                          world.
+                      </p>
+                  </div>
 
-        <Container>
-          <div className={styles.text}>
-            <h1>Support Us</h1>
-            <h2>Donate!</h2>
-            <p>
-              A donation in any amount can greatly help us improve our
-              communities! All donations are tax deductible (up to 60% of
-              the donation) in the United States.
-            </p>
-          </div> 
+                  <div className={styles.content}>
+                      <Image
+                          src="/images/photos/group2.jpg"
+                          alt="Group photo 2"
+                          fill
+                      />
+                  </div>
+              </Container>
 
-          <div className={styles.content} style={{flex:"0 1 50%"}}>
-            <iframe
-               src="https://www.zeffy.com/en-US/embed/donation-form/11f62ca5-4b9c-4569-9a10-22cae20834bd"
-               title="Donate to CORA"
-            />
-          </div>
-        </Container>
+              <Container>
+                  <div className={styles.text}>
+                      <h1>Support Us</h1>
+                      <h2>Donate!</h2>
+                      <p>
+                          A donation in any amount can greatly help us improve
+                          our communities! All donations are tax deductible (up
+                          to 60% of the donation) in the United States.
+                      </p>
+                  </div>
 
-        <Container style={{height: "min-content"}}>
-          <div className={styles.mixed}>
-            <div>
-              <Image src="/images/photos/coraInNews.png" alt="CORA in the news" fill />
-            </div>
-            <Link href="https://www.wdio.com/front-page/top-stories/students-from-cora-group-collected-items-for-proctor-food-shelf/">
-              <h3>CORA in the news</h3>
-            </Link>
+                  <div
+                      className={styles.content}
+                      style={{ flex: "0 1 50%" }}
+                  >
+                      <iframe
+                          src="https://www.zeffy.com/en-US/embed/donation-form/11f62ca5-4b9c-4569-9a10-22cae20834bd"
+                          title="Donate to CORA"
+                      />
+                  </div>
+              </Container>
 
-            <p>
-              CORA Duluth was featured on WDIO for part 1 of their
-              2023 Food Drive!
-            </p>
-            <small>
-              Note: numbers reported only account for part 1 of the food drive, and did not count amount donated to CHUM.
-            </small>
-          </div>
+              <Container style={{ height: "min-content" }}>
+                  <div className={styles.mixed}>
+                      <div>
+                          <Image
+                              src="/images/photos/coraInNews.png"
+                              alt="CORA in the news"
+                              fill
+                          />
+                      </div>
+                      <Link href="https://www.wdio.com/front-page/top-stories/students-from-cora-group-collected-items-for-proctor-food-shelf/">
+                          <h3>CORA in the news</h3>
+                      </Link>
 
-          <div className={styles.mixed}>
-            <div>
-              <Image src="/images/photos/group3.jpg" alt="Group photo 3" fill />
-            </div>
-            <Link href="/join">
-              <h3>Start a branch today</h3>
-            </Link>
-            
-            <p>
-              Apply to start a CORA branch today! Fill out our online application and
-              we will schedule a short interview shortly. 
-            </p>
-            <small>
-              CORA branches are separate organizations and CORA is not liable for their operations.
-            </small>
-          </div>
-        </Container>
+                      <p>
+                          CORA Duluth was featured on WDIO for part 1 of their
+                          2023 Food Drive!
+                      </p>
+                      <small>
+                          Note: numbers reported only account for part 1 of the
+                          food drive, and did not count amount donated to CHUM.
+                      </small>
+                  </div>
 
-        <InteractiveMap locations={[]} />
-      </main>
+                  <div className={styles.mixed}>
+                      <div>
+                          <Image
+                              src="/images/photos/group3.jpg"
+                              alt="Group photo 3"
+                              fill
+                          />
+                      </div>
+                      <Link href="/join">
+                          <h3>Start a branch today</h3>
+                      </Link>
 
-      <Footer />
-    </>
-  ) 
+                      <p>
+                          Apply to start a CORA branch today! Fill out our
+                          online application and we will schedule a short
+                          interview shortly.
+                      </p>
+                      <small>
+                          CORA branches are separate organizations and CORA is
+                          not liable for their operations.
+                      </small>
+                  </div>
+              </Container>
+
+              <InteractiveMap locations={[]} />
+          </main>
+
+          <Footer />
+      </>
+  ); 
 }
 
 function Hero() {
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,34 +4,58 @@ import { BranchesMenuContext } from "@/lib/context";
 import style from "./index.module.css";
 import Image from "next/image";
 import Link from "next/link"
-import { useContext } from "react";
+import { useContext, useState, useRef, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
-
     const { setIsBranchesOpen } = useContext(BranchesMenuContext);
 
+    const [isActive, setIsActive] = useState(usePathname() != "/");
+
+    function handleScroll() {
+        setIsActive(
+            window.scrollY >
+                Math.max(
+                    document.documentElement.clientHeight || 0,
+                    window.innerHeight || 0
+                )
+        );
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
     return (
-        <div className={style.main}>
-            <div className={style.content}> 
+        <div className={style.main + (isActive ? ` ${style.active}` : "")}>
+            <div className={style.content}>
                 <div className={style.logo}>
-                    <Image src="/logo.png" alt="CORA logo" fill={true} />
-                </div> 
-                
-                <div className={style.links}>
-                    <Link href="/about">About</Link> 
-                    <Link href="/contact">Contact</Link>  
-                    <Link href="/donate">Donate</Link>
-                    <Link href="/join">Join</Link> 
+                    <Image
+                        src="/logo.png"
+                        alt="CORA logo"
+                        fill={true}
+                    />
                 </div>
 
-                <button className={style.branches} onClick={() => setIsBranchesOpen(true)}>
+                <div className={style.links}>
+                    <Link href="/about">About</Link>
+                    <Link href="/contact">Contact</Link>
+                    <Link href="/donate">Donate</Link>
+                    <Link href="/join">Join</Link>
+                </div>
+
+                <button
+                    className={style.branches}
+                    onClick={() => setIsBranchesOpen(true)}
+                >
                     Branches
                 </button>
 
-                <Link href="/student" className={style.signIn}>
+                <Link
+                    href="/student"
+                    className={style.signIn}
+                >
                     Sign in
                 </Link>
             </div>
         </div>
-    )
+    );
 }
